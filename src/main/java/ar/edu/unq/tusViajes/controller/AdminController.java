5644c564e91be9ab20d196aad581d1c4a9a07d1a
thiago.sofarelli <sofarelli.thiago10@gmail.com>
2026-09-08 10:41:57 -0300

    Deleted unnecessary admins format

--- src/main/java/ar/edu/unq/tusViajes/controller/AdminController.java
+++ src/main/java/ar/edu/unq/tusViajes/controller/AdminController.java
@@ -18,7 +18,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping({"/api/admin/administrators", "/api/admin/admins"})
+@RequestMapping({"/api/admin/administrators"})
 @RequiredArgsConstructor
 public class AdminController {
 
