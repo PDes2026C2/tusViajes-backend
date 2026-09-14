049f82ba57c8bae574b1c186bccbac0154b17280
thiago.sofarelli <sofarelli.thiago10@gmail.com>
2026-09-14 00:52:30 -0300

    Added entity manager for Flights and Cities relations

--- src/main/java/ar/edu/unq/tusViajes/service/FlightService.java
+++ src/main/java/ar/edu/unq/tusViajes/service/FlightService.java
@@ -4,10 +4,10 @@ import ar.edu.unq.tusViajes.adapters.dto.CityDTO;
 import ar.edu.unq.tusViajes.adapters.dto.FlightDTO;
 import ar.edu.unq.tusViajes.controller.dto.response.FlightResponseDTO;
 import ar.edu.unq.tusViajes.model.City;
-import ar.edu.unq.tusViajes.model.Country;
 import ar.edu.unq.tusViajes.model.Flight;
 import ar.edu.unq.tusViajes.repository.FlightRepository;
 import ar.edu.unq.tusViajes.validator.EntityValidator;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +22,7 @@ public class FlightService {
     private final EntityValidator entityValidator;
     private final FlightRepository flightRepository;
     private final FlightsApiService flightsApiService;
+    private final EntityManager entityManager;
 
     @Transactional(readOnly = true)
     public List<FlightResponseDTO> getAll() {
@@ -59,11 +60,15 @@ public class FlightService {
     public Flight getOrCreateFlight(Long flightId) {
         if (!flightRepository.existsById(flightId)) {
             FlightDTO flightDTO = flightsApiService.getFlight(flightId);
+
+            City originCity = entityManager.getReference(City.class, flightDTO.originCity().id());
+            City destinationCity = entityManager.getReference(City.class, flightDTO.destinationCity().id());
+
             return flightRepository.save(new Flight(
                     flightDTO.id(),
                     flightDTO.airline(),
-                    CityDTO.to(flightDTO.originCity()),
-                    CityDTO.to(flightDTO.destinationCity()),
+                    originCity,
+                    destinationCity,
                     flightDTO.departureDate(),
                     flightDTO.arrivalDate()));
         }
